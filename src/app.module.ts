2fb59e10a808d3899215179b5780e2464595101b
Chung Phong <phonghoang0205@gmail.com>
2025-01-03 01:39:28 +0700

    #18. Sử dụng MongoDB với NestJS

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://phonghoang02052001:9bNf3uqRZjhANh2j@chungphong.nawtq.mongodb.net/',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
