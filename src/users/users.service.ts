056f81d6785b56469484182990c9a50593287583
Chung Phong <phonghoang0205@gmail.com>
2025-01-03 14:00:18 +0700

    #23. Get User by Id

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,11 +2,9 @@ import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import mongoose, { Model } from 'mongoose';
 import { User } from './schemas/user.schema';
 import { genSaltSync, hashSync } from 'bcryptjs';
-
-
 
 @Injectable()
 export class UsersService {
@@ -22,8 +20,6 @@ export class UsersService {
   };
 
   async create(createUserDto: CreateUserDto) {
-
-  
     const hashPassword = this.getHashPassword(createUserDto.password);
     let user = await this.userModel.create({
       email: createUserDto.email,
@@ -37,8 +33,11 @@ export class UsersService {
     return `This action returns all users`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  findOne(id: string) {
+    if (!mongoose.Types.ObjectId.isValid(id)) return `not found user`;
+    return this.userModel.findOne({
+      _id: id,
+    });
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
