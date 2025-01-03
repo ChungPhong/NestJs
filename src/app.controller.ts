20882824236333acbbc360bd3f7a589bfbabc1a0
Chung Phong <phonghoang0205@gmail.com>
2025-01-04 03:22:40 +0700

    #40. Implementing Passport JWT

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -11,6 +11,7 @@ import { ConfigService } from '@nestjs/config';
 import { AuthGuard } from '@nestjs/passport';
 import { LocalAuthGuard } from './auth/local-auth.guard';
 import { AuthService } from './auth/auth.service';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
 
 @Controller()
 export class AppController {
@@ -20,10 +21,16 @@ export class AppController {
     private authService: AuthService,
   ) {}
 
-  @UseGuards(LocalAuthGuard)
+  @UseGuards(LocalAuthGuard) //middleware người dùng phải gửi đúng username và password
   @Post('/login')
   handleLogin(@Request() req) {
     return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
   }
 
   // @Get()
