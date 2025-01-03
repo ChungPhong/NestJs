12dd02c27f4d228a59527f683f26996104e317e4
Chung Phong <phonghoang0205@gmail.com>
2025-01-03 22:07:58 +0700

    #38. LocalGuard với Passport

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,15 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Render,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 import { AppService } from './app.service';
 import { ConfigService } from '@nestjs/config';
+import { AuthGuard } from '@nestjs/passport';
+import { LocalAuthGuard } from './auth/local-auth.guard';
 
 @Controller()
 export class AppController {
@@ -9,16 +18,21 @@ export class AppController {
     private configService: ConfigService,
   ) {}
 
-  @Get()
-  @Render('home')
-  handleHomePage() {
-    //port from .env
-    console.log('>>>check port = ', this.configService.get<string>('PORT'));
-
-    const message = this.appService.getHello();
-    return {
-      message: message,
-    };
-    // return 'this.appService.getHello()';
+  @UseGuards(LocalAuthGuard)
+  @Post('/login')
+  handleLogin(@Request() req) {
+    return req.user;
   }
+
+  // @Get()
+  // @Render('home')
+  // handleHomePage() {
+  //   //port from .env
+  //   console.log('>>>check port = ', this.configService.get<string>('PORT'));
+  //   const message = this.appService.getHello();
+  //   return {
+  //     message: message,
+  //   };
+  //   // return 'this.appService.getHello()';
+  // }
 }
