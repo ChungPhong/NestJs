70d8ee04946ac1b0a70ea4f88d72c0b0b7903dbf
Chung Phong <phonghoang0205@gmail.com>
2025-01-04 17:34:19 +0700

    #48. Soft-delete plugin(Xóa mềm)

--- src/users/schemas/user.schema.ts
+++ src/users/schemas/user.schema.ts
@@ -28,6 +28,12 @@ export class User {
 
   @Prop()
   updateAt: string;
+
+  @Prop()
+  isDeleted: boolean;
+
+  @Prop()
+  deleteAt: Date;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
