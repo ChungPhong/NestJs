e9a9fa55bdd658f4e429bfff1c1dd84be98bfc84
Chung Phong <phonghoang0205@gmail.com>
2025-01-06 20:22:26 +0700

    #68. API Get Account (F5 Refresh)

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -9,10 +9,11 @@ import {
   UseGuards,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { Public, ResponseMessage } from 'src/decorator/customize';
+import { Public, ResponseMessage, User } from 'src/decorator/customize';
 import { LocalAuthGuard } from './local-auth.guard';
 import { RegisterUserDto } from 'src/users/dto/create-user.dto';
 import { Request, Response } from 'express';
+import { IUser } from 'src/users/users.interface';
 
 @Controller('auth') // route
 export class AuthController {
@@ -25,12 +26,19 @@ export class AuthController {
   handleLogin(@Req() req, @Res({ passthrough: true }) response: Response) {
     return this.authService.login(req.user, response);
   }
+
   // @UseGuards(JwtAuthGuard) // phải gửi kèm theo accesstoken
   @Public()
   @ResponseMessage('Register a new user')
   @Post('/register')
   handleRegister(@Body() registerUserDto: RegisterUserDto) {
     return this.authService.register(registerUserDto);
+  }
+
+  @ResponseMessage('Get user information')
+  @Get('/account')
+  handleGetAccount(@User() user: IUser) {
+    return { user };
   }
 
   // @Get()
