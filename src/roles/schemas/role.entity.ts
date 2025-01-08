80170c66f0be71141111c624ede9addd0fef31fa
Chung Phong <phonghoang0205@gmail.com>
2025-01-08 23:46:30 +0700

    #84. Tạo Modules Permission & Roles

---
+++ src/roles/schemas/role.entity.ts
@@ -0,0 +1,55 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { HydratedDocument } from 'mongoose';
+import { Company } from 'src/companies/schemas/company.schemas';
+import { Job } from 'src/jobs/schemas/job.entity';
+import { Permission } from 'src/permissions/schemas/permission.entity';
+
+export type RoleDocument = HydratedDocument<Role>;
+
+@Schema({ timestamps: true })
+export class Role {
+  @Prop()
+  name: string;
+
+  @Prop()
+  description: string;
+
+  @Prop()
+  isActive: boolean;
+
+  @Prop({ type: [mongoose.Schema.Types.ObjectId], ref: Permission.name })
+  permission: mongoose.Schema.Types.ObjectId;
+
+  @Prop({ type: Object })
+  createdBy: {
+    _id: mongoose.Schema.Types.ObjectId;
+    email: string;
+  };
+
+  @Prop({ type: Object })
+  updatedBy: {
+    _id: mongoose.Schema.Types.ObjectId;
+    email: string;
+  };
+
+  @Prop({ type: Object })
+  deletedBy: {
+    _id: mongoose.Schema.Types.ObjectId;
+    email: string;
+  };
+
+  @Prop()
+  createdAt: Date;
+
+  @Prop()
+  updateAt: Date;
+
+  // 2 trường cuối là dành cho soft delete
+  @Prop()
+  isDeleted: boolean;
+
+  @Prop()
+  deleteAt: Date;
+}
+
+export const RoleSchema = SchemaFactory.createForClass(Role);
