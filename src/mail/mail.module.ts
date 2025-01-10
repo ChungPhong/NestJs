26ce45d4895852735b44b3ba381fabffc837d0cc
Chung Phong <phonghoang0205@gmail.com>
2025-01-10 22:48:54 +0700

    #101. Fetch Subscribers by skills

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -1,9 +1,14 @@
+import { MongooseModule } from '@nestjs/mongoose';
 import { Module } from '@nestjs/common';
 import { MailService } from './mail.service';
 import { MailController } from './mail.controller';
 import { ConfigService } from '@nestjs/config';
 import { MailerModule } from '@nestjs-modules/mailer';
-
+import { join } from 'path';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
+import { Subscriber } from 'rxjs';
+import { SubscriberSchema } from 'src/subscribers/schemas/subscriber.schema';
+import { Job, JobSchema } from 'src/jobs/schemas/job.entity';
 // mail.module.ts
 @Module({
   imports: [
@@ -17,17 +22,24 @@ import { MailerModule } from '@nestjs-modules/mailer';
             pass: configService.get<string>('EMAIL_AUTH_PASS'),
           },
         },
-        // template: {
-        //   dir: join(__dirname, 'templates'),
-        //   adapter: new HandlebarsAdapter(),
-        //   options: {
-        //     strict: true,
-        //   },
-        // },
+        template: {
+          dir: join(__dirname, 'templates'),
+          adapter: new HandlebarsAdapter(),
+          options: {
+            strict: true,
+          },
+        },
+        preview:
+          configService.get<string>('EMAIL_PREVIEW') === 'true' ? true : false,
       }),
       inject: [ConfigService],
     }),
+    MongooseModule.forFeature([
+      { name: Subscriber.name, schema: SubscriberSchema },
+      { name: Job.name, schema: JobSchema },
+    ]),
   ],
+
   controllers: [MailController],
   providers: [MailService],
 })
