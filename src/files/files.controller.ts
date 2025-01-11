3d481a824282187623818885ae7882fd4a80b401
Chung Phong <phonghoang0205@gmail.com>
2025-01-11 11:04:03 +0700

    #112. Fix Bugs Upload file Còn Tồn Đọng

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -10,6 +10,7 @@ import {
   UploadedFile,
   ParseFilePipeBuilder,
   HttpStatus,
+  UseFilters,
 } from '@nestjs/common';
 import { FilesService } from './files.service';
 import { CreateFileDto } from './dto/create-file.dto';
@@ -17,6 +18,7 @@ import { UpdateFileDto } from './dto/update-file.dto';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { Public, ResponseMessage } from 'src/decorator/customize';
 import { ApiTags } from '@nestjs/swagger';
+import { HttpExceptionFilter } from 'src/core/http-exception.filter';
 
 @ApiTags('files') //Operations Swagger
 @Controller('files')
@@ -27,20 +29,9 @@ export class FilesController {
   @Post('upload')
   @ResponseMessage('Upload single file')
   @UseInterceptors(FileInterceptor('fileUpload'))
+  @UseFilters(new HttpExceptionFilter())
   uploadFile(
-    @UploadedFile(
-      new ParseFilePipeBuilder() //validate upload file
-        .addFileTypeValidator({
-          fileType:
-            '.(png|jpeg|jpg|pdf|text/plain|doc|docx|xlsx|xls|svg|pptx|ppt|gif|csv|)',
-        })
-        .addMaxSizeValidator({
-          maxSize: 1024 * 1024,
-        })
-        .build({
-          errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
-        }),
-    )
+    @UploadedFile()
     file: Express.Multer.File,
   ) {
     return {
